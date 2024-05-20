5748d428eb66189c6a5187d9f34f2b529f7b098e
MasterFHC <MasterFHC@sjtu.edu.cn>
2024-05-20 20:54:39 +0800

    everything except it++--

--- AVL/src.hpp
+++ AVL/src.hpp
@@ -2,10 +2,11 @@
 #define SJTU_AVLTree
 #include <cstddef>
 #include <queue>
+#include <iostream>
 
 template<class T, class Compare = std::less<T>>
 class AVL {
-private:
+public:
     typedef struct Node {
         /*credit to
         https://ivanzz1001.github.io/records/post/data-structure/2018/06/14/ds-avl_tree
@@ -18,36 +19,64 @@ private:
         struct Node* right;
         Node(int height, int size, T* data, struct Node* left, struct Node* right)
             :height(height), size(size), data(data), left(left), right(right) {}
+        Node(const Node& other){
+            height = other.height;
+            size = other.height;
+            data = new T(*(other.data));
+            left = other.left;
+            right = other.right;
+        }
         Node() {
             left = right = nullptr;
             delete data;
             data = nullptr;
         }
     }AVLNode, * AVLTree;
+private:
     int size = 0;
     inline int getHeight(AVLNode* node) {
         if (!node) return 0;
         return node->height;
     }
+    inline int getSize(AVLNode* node) {
+        if (!node) return 0;
+        return node->size;
+    }
     inline void refreshHeight(AVLNode* node) {
         node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
+        node->size = 1 + getSize(node->left) + getSize(node->right);
+    }
+    AVLNode* findMinNode(AVLNode* node) {
+        while (node->left) node = node->left;
+        return node;
+    }
+    AVLNode* findMaxNode(AVLNode* node) {
+        while (node->right) node = node->right;
+        return node;
     }
 public:
     AVLNode* root = nullptr;
-    AVL(){
-
-    }
-    ~AVL(){
+    AVL() {}
+    ~AVL() {
         std::queue<AVLNode*> q;
-        if(!root) return;
+        if (!root) return;
         q.push(root);
-        while(!q.empty()){
+        while (!q.empty()) {
             auto now = q.front();
+            // std::cout<<now<<std::endl;
             q.pop();
-            if(now->left) q.push(now->left);
-            if(now->right) q.push(now->right);
+            if (now->left) {
+                q.push(now->left);
+                // std::cout<<"pushed left="<<now->left<<std::endl;
+            }
+            if (now->right) {
+                q.push(now->right);
+                // std::cout<<"pushed right="<<now->right<<std::endl;
+            }
+            delete now->data;
             delete now;
         }
+        std::cout << "end of avl" << std::endl;
     }
     void leftRotate(AVLTree* node) {
         AVLNode* X = *node, * XR = X->right;
@@ -63,25 +92,21 @@ public:
         *node = XL;
         refreshHeight(X), refreshHeight(XL);
     }
-    AVLNode* find(AVLNode* node, T data) {
+    AVLNode* find(AVLNode* node, const T& data) {
         if (!node) return nullptr;
         if (*(node->data) == data) return node;
         if (Compare()(data, *(node->data))) {
-            if (node->left) return find(node->left);
+            if (node->left) return find(node->left, data);
             else return nullptr;
         }
         else {
-            if (node->right) return find(node->right);
+            if (node->right) return find(node->right, data);
             else return nullptr;
         }
     }
-    std::pair<AVLTree*, bool> insert(AVLTree* node, T data) {
+    std::pair<AVLTree*, bool> insert(AVLTree* node, const T& data) {
         if (!*node) {
-            auto created = new AVLNode(1, 1, new T(data), nullptr, nullptr);
-            node = &created;
-            // root = created;
-            // std::cout<<root<<std::endl;
-            // std::cout<<node<<std::endl;
+            *node = new AVLNode(1, 1, new T(data), nullptr, nullptr);
             return { node, true };
         }
         if (*((*node)->data) == data) return { node, false };
@@ -106,7 +131,7 @@ public:
             if (!ret.second) return { ret.first, false };
             int lheight = getHeight((*node)->left), rheight = getHeight((*node)->right);
             if (rheight - lheight == 2) {
-                if (Compare()(data, *((*node)->left->data))) {//RL
+                if (Compare()(data, *((*node)->right->data))) {//RL
                     rightRotate(&((*node)->right));
                     leftRotate(node);
                 }
@@ -118,21 +143,178 @@ public:
             return ret;
         }
     }
-    bool erase(AVLTree* node);
+    bool erase(AVLTree* node, const T& data) {
+        if (!(*node)) return false;
+        if (*((*node)->data) == data) {
+            if ((*node)->left and (*node)->right) {
+                auto key = *(*(findMinNode((*node)->right))->data);
+                *((*node)->data) = key;
+                erase(&((*node)->right), key);
+                int lheight = getHeight((*node)->left), rheight = getHeight((*node)->right);
+                if (lheight - rheight == 2) {
+                    if (getHeight((*node)->left->left) >= getHeight((*node)->left->right)) {//LL
+                        rightRotate(node);
+                    }
+                    else {//LR
+                        leftRotate(&((*node)->left));
+                        rightRotate(node);
+                    }
+                }
+                refreshHeight(*node);
+                return true;
+            }
+            else if (!(*node)->left) {
+                auto now = *node;
+                *node = now->right;
+                delete now->data;
+                delete now;
+                now = nullptr;
+                return true;
+            }
+            else {
+                auto now = *node;
+                *node = now->left;
+                delete now->data;
+                delete now;
+                now = nullptr;
+                return true;
+            }
+        }
+        else if (Compare()(data, *((*node)->data))) {//erase in left subtree
+            if (!erase(&((*node)->left), data)) {
+                return false;
+            }
+            int lheight = getHeight((*node)->left), rheight = getHeight((*node)->right);
+            if (rheight - lheight == 2) {
+                if (getHeight((*node)->right->right) >= getHeight((*node)->right->left)) {//RR
+                    leftRotate(node);
+                }
+                else {//RL
+                    rightRotate(&((*node)->right));
+                    leftRotate(node);
+                }
+            }
+            refreshHeight(*node);
+            return true;
+        }
+        else {//erase in right subtree
+            if (!erase(&((*node)->right), data)) {
+                return false;
+            }
+            int lheight = getHeight((*node)->left), rheight = getHeight((*node)->right);
+            if (lheight - rheight == 2) {
+                if (getHeight((*node)->left->left) >= getHeight((*node)->left->right)) {//LL
+                    rightRotate(node);
+                }
+                else {//LR
+                    leftRotate(&((*node)->left));
+                    rightRotate(node);
+                }
+            }
+            refreshHeight(*node);
+            return true;
+        }
+    }
+
+    /*
+    **function for checking correctness and utilities
+    */
+    void traverse(AVLTree node) {
+        if (!node) {
+            std::cout << "TREE IS EMPTY!" << std::endl;
+            return;
+        }
+        std::cout << *(node->data) << ' ' << (node->size) << std::endl;
+        if (node->left) traverse(node->left);
+        if (node->right) traverse(node->right);
+    }
+    AVLNode* findLowerBound(AVLNode* node, const T& key) {
+        if (key == *((*node)->data) or (!(node->left) and !(node->right))) return node;
+        else if (Compare()(key, *((*node)->data)))
+            return findLowerBound(node->left, key);
+        else return findLowerBound(node->right, key);
+    }
+    AVLNode* findUpperBound(AVLNode* node, const T& key) {
+        if (!(node->left) and !(node->right)) return node;
+        else if (Compare()(key, *((*node)->data))) {
+            auto now = node;
+            auto foundKey = *(findMaxNode(node->left)->data);
+            if (Compare()(foundKey, key) or foundKey == key) {
+                return node;
+            }
+            else {
+                return findUpperBound(node->left, key);
+            }
+        }
+        else {
+            return findUpperBound(node->right, key);
+        }
+    }
+    inline bool inRange(const T& data, const T& l, const T& r) const {
+        return (Compare()(l, *(*node)->data) or l == *(*node)->data) and
+            (Compare()(*(*node)->data, r) or r == *(*node)->data)
+    }
+    size_t findRange(AVLNode* node, const T& l, const T& r) const {
+        int ret = 0;
+        if (inRange(*((*node)->data), l, r)) {
+            ++ret;
+            if(node->left) ret += findRange(node->left);
+            if(node->right) ret += findRange(node->right);
+        }
+        else {
+            if(Compare()(*(*node)->data, l)){
+                ret += findRange(node->right);
+            }
+            else{
+                ret += findRange(node->left);
+            }
+        }
+        return ret;
+    }
 };
 
 template<class T, class Compare = std::less<T>>
 class ESet {
-    ESet();
-    ~ESet();
+private:
+    AVL<T>* table = nullptr;
+    inline void fixBeginEnd(){
+        beginIt = iterator(table->findMinNode(table->root));
+        endIt = iterator(table->findMaxNode(table->root));
+    }
+public:
+    iterator beginIt, endIt;//actually, this is a fake endIt(the one before end)
+    ESet() {
+        table = new AVL<T>();
+        beginIt = endIt = iterator(nullptr);
+    }
+    ~ESet() {
+        delete table;
+        table = nullptr;
+    }
     class iterator {
-        iterator();
+    public:
+        typename AVL<T>::AVLNode* node;
+        iterator() {}
+        iterator(typename AVL<T>::AVLNode* node) : node(node) {}
 
-        T& operator*() const;
-        T* operator->() const noexcept;
-
-        bool operator==(const iterator& rhs) const;
-        bool operator!=(const iterator& rhs) const;
+        T& operator*() const {
+            if (!node) throw("iterator pointing to null");
+            return *(node->data);
+        }
+        T* operator->() const noexcept {
+            if (!node) throw("iterator pointing to null");
+            return node->data;
+        }
+        T* get() const noexcept {
+            if (!node) return nullptr;
+            return node->data;
+        }
+        bool operator==(const iterator& rhs) const {
+            return node == rhs.node;
+        }
+        bool operator!=(const iterator& rhs) const {
+            return node != rhs.node;
+        }
         //it++
         iterator operator++(int);
         //++it
@@ -142,20 +324,61 @@ class ESet {
         //--it
         iterator operator--();
     };
+    std::pair<iterator, bool> emplace(T&& args) {
+        auto ret = table->insert(&(table->root), std::forward<T>(args));
+        fixBeginEnd();
+        return std::make_pair(iterator(*(ret.first)), ret.second);
+    }
     template< class... Args >
-    std::pair<iterator, bool> emplace(Args&&... args);
-    size_t erase(const T& key);
-    iterator find(const T& key) const;
-    ESet(const ESet& other);
+    std::pair<iterator, bool> emplace(Args&&... args) {
+        auto ret = table->insert(&(table->root), std::forward<T>(args)...);
+        fixBeginEnd();
+        return std::make_pair(iterator(*(ret.first)), ret.second);
+    }
+    size_t erase(const T& key) {
+        auto ret = table->erase(&(table->root), key);
+        fixBeginEnd();
+        return ret;
+    }
+    iterator find(const T& key) const {
+        auto ret = table->find(table->root, key);
+        if (ret) return iterator(ret);
+        else return end();
+    }
+    ESet(const ESet& other){
+        table = new AVL<T>();
+        std::queue<typename AVL<T>::AVLNode*&> qthis, qother;
+        qthis.push(table->root), qother.push(other->root);
+        while(!qother.empty()){
+            auto nowthis = qthis.front(), nowother = qother.front();
+            qthis.pop(), qother.pop();
+            nowthis
+        }
+        fixBeginEnd();
+    }
     ESet& operator=(const ESet& other);
     ESet(ESet&& other);
     ESet& operator=(ESet&& other) noexcept;
-    size_t range(const T& l, const T& r) const;
-    size_t size() const noexcept;
-    iterator lower_bound(const T& key) const;
-    iterator upper_bound(const T& key) const;
-    iterator begin() const noexcept;
-    iterator end() const noexcept;
+
+    size_t range(const T& l, const T& r) const {
+        if (Compare()(r, l)) return 0;
+        return findRange(table->root, l, r);
+    }
+    size_t size() const noexcept {
+        return table->getSize(table->root);
+    }
+    iterator lower_bound(const T& key) const {
+        return iterator(table->findLowerBound(table->root, key));
+    }
+    iterator upper_bound(const T& key) const {
+        return iterator(table->findUpperBound(table->root, key));
+    }
+    iterator begin() const noexcept {
+        return beginIt;
+    }
+    iterator end() const noexcept {
+        return iterator(nullptr);
+    }
 };
 
 #endif
--- AVL/test.cpp
+++ AVL/test.cpp
@@ -3,9 +3,35 @@
 #include "src.hpp"
 using namespace std;
 int main(){
-    AVL<int> tree;
-    tree.insert(&(tree.root), 1);
-    cout<<tree.root<<endl;
+    ESet<int> test;
+    for(int i=1;i<=10;i++){
+        test.emplace(i);
+    }
+    for(int i=1;i<=10;i++){
+        test.erase(i);
+        for(int j=1;j<=10;j++){
+            auto ret = test.find(j);
+            if(ret.get()) cout<<"found"<<' ';
+            // else cout<<"notfound"<<' ';
+        }
+        cout<<endl;
+    }
+
+    // AVL<int> test;
+    // for(int i=1;i<=10;i++){
+    //     test.insert(&(test.root), i);
+    //     test.traverse(test.root);
+    //     cout<<"end of this traverse"<<endl;
+    //     cin.get();
+    // }
+    // for(int i=1;i<=10;i++){
+    //     test.erase(&(test.root), i);
+    //     test.traverse(test.root);
+    //     cout<<"end of this traverse"<<endl;
+    //     cin.get();
+    // }
+
+    // cin.get();
     // if(tree.root == nullptr) cout<<"fuck";
     return 0;
 }
